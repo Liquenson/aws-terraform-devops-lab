4cfbe8bd4806f1ca14965e6c00fc06a6171323aa
Liquenson <liquensonrubenalexis@gmail.com>
2026-03-19 21:49:48 +0100

    refactor(terraform): remove local state and ignore terraform artifacts

--- terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "devops-lab-tfstate-538079272432"
-    key            = "dev/terraform.tfstate"
-    region         = "eu-west-1"
-    dynamodb_table = "devops-lab-state-lock"
-    encrypt        = true
-  }
-}
