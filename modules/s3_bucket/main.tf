558265e35191274ca748cefef8dc3e02d50505e7
Liquenson <liquensonrubenalexis@gmail.com>
2026-03-19 21:33:21 +0100

    feat(terraform): add S3 remote state backend with DynamoDB locking in eu-west-1

--- modules/s3_bucket/main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_s3_bucket" "devops_lab_bucket" {
-  bucket = var.bucket_name
-
-  tags = {
-    Name        = "DevOpsLabBucket"
-    Environment = var.environment
-  }
-}
--- modules/s3_bucket/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "bucket_name" {
-  description = "Nombre del bucket S3"
-  type        = string
-}
-
-variable "environment" {
-  description = "Entorno del proyecto"
-  type        = string
-}
